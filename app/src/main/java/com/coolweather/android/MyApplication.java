84bbfb4a9d37c019c0605e6ec402374f4d9d30e0
Xufei <Xufei@gmail.com>
2019-02-24 18:52:27 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/coolweather/android/MyApplication.java
@@ -0,0 +1,20 @@
+package com.coolweather.android;
+
+import android.app.Application;
+import android.content.Context;
+
+import org.litepal.LitePal;
+
+public class MyApplication extends Application {
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LitePal.initialize(this);
+        context=getApplicationContext();
+    }
+    public static Context getContext(){
+        return context;
+    }
+}
